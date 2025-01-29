1d9aa194554f8a1a4dd9853da811ac771e949c7d
eeprykhodko <94558600+eeprykhodko@users.noreply.github.com>
2025-01-29 03:01:33 +0200

    added logic for distance check; added hint to step back; updated suits models (in progress); added logic for suits switch;

---
+++ houseboat_museum_v01/Cache/6df854e9-c624-48b0-abc0-5a1dec7fded1/24aa23d1d6c2a0856e5c3d0582acc0bf/Data/DistanceController.js
@@ -0,0 +1,4 @@
+// @input SceneObject distanceHint
+// @input Component.ObjectTracking3D track
+// @input SceneObject manSuitTween
+// @input SceneObject womanSuitTween
---
+++ houseboat_museum_v01/Cache/6df854e9-c624-48b0-abc0-5a1dec7fded1/d51fef4b6fd04c7bc205f942c7f11c68/Data/DistanceController.js
@@ -0,0 +1,2 @@
+// @input SceneObject distanceHint
+// @input Component.ObjectTracking3D track
