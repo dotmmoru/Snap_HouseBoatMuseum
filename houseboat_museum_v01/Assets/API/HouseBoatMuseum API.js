95603f34452711b91336c321a59d601af986d6da
PavelIvasyuk <dotmmoru@gmail.com>
2025-07-21 22:04:52 +0300

    removed live camera - pushed to QA

--- houseboat_museum_v01/Assets/API/HouseBoatMuseum API.js
+++ houseboat_museum_v01/Assets/API/HouseBoatMuseum API.js
@@ -7,38 +7,38 @@ const ApiModule = new Module.ApiModule(script.remoteServiceModule);
 var eventDone = false;
 
 script.api.trigger = function () {
-    TriggerEvent();
+    //  TriggerEvent();
 }
 
 function TriggerEvent() {
-    if (eventDone)
-        return;
-    eventDone = true;
+    // if (eventDone)
+    //     return;
+    // eventDone = true;
 
-    print('capture');
+    // print('capture');
 
-    delay_ResetEventDone.reset(1.0);
+    // delay_ResetEventDone.reset(1.0);
 
-    // Example of calling an endpoint in ApiModule, replace with actual endpoint name
-    ApiModule.event_triggered({
-        // There might be required parameters
-        parameters: {
-            "parameter1": "12345"  // Replace with actual parameter names and values
-        },
-        // Body might be optional
-        body: JSON.stringify({
-            "additionalInfo": "Some info"  // Adjust based on the actual endpoint requirements
-        })
-    }).then((response) => {
-        // Parse response as JSON string and log it
-        print("Response metadata: " + JSON.stringify(response.metadata))
-        print("Response body: " + response.bodyAsString());
-    }).catch((error) => {
-        print(error + "\n" + error.stack);
-    });
+    // // Example of calling an endpoint in ApiModule, replace with actual endpoint name
+    // ApiModule.event_triggered({
+    //     // There might be required parameters
+    //     parameters: {
+    //         "parameter1": "12345"  // Replace with actual parameter names and values
+    //     },
+    //     // Body might be optional
+    //     body: JSON.stringify({
+    //         "additionalInfo": "Some info"  // Adjust based on the actual endpoint requirements
+    //     })
+    // }).then((response) => {
+    //     // Parse response as JSON string and log it
+    //     print("Response metadata: " + JSON.stringify(response.metadata))
+    //     print("Response body: " + response.bodyAsString());
+    // }).catch((error) => {
+    //     print(error + "\n" + error.stack);
+    // });
 }
 
 var delay_ResetEventDone = script.createEvent("DelayedCallbackEvent");
 delay_ResetEventDone.bind(function (eventData) {
-    eventDone = false;
+    // eventDone = false;
 });
